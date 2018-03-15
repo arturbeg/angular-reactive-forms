2b6bd0ac004c9fbf608651d402af327ada5bfefa
Artur Begyan <arturbegyan98@gmail.com>
2018-03-15 19:43:23 +0000

    final

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,22 @@ import { AppComponent } from './app.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 import {ReactiveFormsModule} from '@angular/forms';
+import { HeroListComponent } from './hero-list/hero-list.component';
+
+
+import {HeroService} from './hero.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    HeroListComponent
   ],
   imports: [
     BrowserModule,
     ReactiveFormsModule,
   ],
-  providers: [],
+  providers: [HeroService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,9 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges }       from '@angular/core';
+import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
 
-import {Validators, FormGroup, FormBuilder} from '@angular/forms';
-
-import {states} from '../data-model';
-
+import { Address, Hero, states } from '../data-model';
+import { HeroService }           from '../hero.service';
 
 @Component({
   selector: 'app-hero-detail',
@@ -11,43 +10,86 @@ import {states} from '../data-model';
   styleUrls: ['./hero-detail.component.css']
 })
 
+export class HeroDetailComponent implements OnChanges {
+  @Input() hero: Hero;
 
-export class HeroDetailComponent {
-	heroForm: FormGroup;
-	states = states
+  heroForm: FormGroup;
+  nameChangeLog: string[] = [];
+  states = states;
 
-	constructor(private fb: FormBuilder) {
+  constructor(
+    private fb: FormBuilder,
+    private heroService: HeroService) {
 
-		this.createForm();
-	}
+    this.createForm();
+    this.logNameChange();
+  }
 
+  createForm() {
+    this.heroForm = this.fb.group({
+      name: '',
+      secretLairs: this.fb.array([]),
+      power: '',
+      sidekick: ''
+    });
+  }
 
-	createForm(){
-		this.heroForm = this.fb.group({ // <-- the parent FormGroup
-			name: ['', Validators.required], // <--- the FormControl called 'name',
-			address: this.fb.group({ // <-- the child form group
-				street: '',
-				city: '',
-				state: '',
-				zip: '',
-			}),
+  ngOnChanges() {
+    this.rebuildForm();
+  }
 
-			power: '',
-			sidekick: '',
+  rebuildForm() {
+    this.heroForm.reset({
+      name: this.hero.name
+    });
+    this.setAddresses(this.hero.addresses);
+  }
 
+  get secretLairs(): FormArray {
+    return this.heroForm.get('secretLairs') as FormArray;
+  };
 
-		});
-	}
+  setAddresses(addresses: Address[]) {
+    const addressFGs = addresses.map(address => this.fb.group(address));
+    const addressFormArray = this.fb.array(addressFGs);
+    this.heroForm.setControl('secretLairs', addressFormArray);
+  }
+
+  addLair() {
+    this.secretLairs.push(this.fb.group(new Address()));
+  }
+
+  onSubmit() {
+    this.hero = this.prepareSaveHero();
+    this.heroService.updateHero(this.hero).subscribe(/* error handling */);
+    this.rebuildForm();
+  }
+
+  prepareSaveHero(): Hero {
+    const formModel = this.heroForm.value;
+
+    // deep copy of form model lairs
+    const secretLairsDeepCopy: Address[] = formModel.secretLairs.map(
+      (address: Address) => Object.assign({}, address)
+    );
+
+    // return new `Hero` object containing a combination of original hero value(s)
+    // and deep copies of changed form model values
+    const saveHero: Hero = {
+      id: this.hero.id,
+      name: formModel.name as string,
+      // addresses: formModel.secretLairs // <-- bad!
+      addresses: secretLairsDeepCopy
+    };
+    return saveHero;
+  }
+
+  revert() { this.rebuildForm(); }
+
+  logNameChange() {
+    const nameControl = this.heroForm.get('name');
+    nameControl.valueChanges.forEach(
+      (value: string) => this.nameChangeLog.push(value)
+    );
+  }
 }
-
-
-
-/*
-	FormBuilder.group is a factory method that creates a FormGroup, it takes an object whose keys and values are FormControl names and their definitions 
-
-*/
-
-/*
-	Reactive validators are simple functions
-
-*/
